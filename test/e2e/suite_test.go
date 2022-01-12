649443357ef76d807be19e830370cc74842e0e01
Miles-Garnsey <miles.garnsey@datastax.com>
2022-01-12 12:40:55 +1100

    Update `framework.WaitForCassOperatorToBeReady()` so that it looks in the `cass-operator` NS, because we've moved to cluster-scoped cass-operator.

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/k8ssandra/k8ssandra-operator/pkg/labels"
 	"os"
 	"path/filepath"
 	"testing"
 	"time"
+
+	"github.com/k8ssandra/k8ssandra-operator/pkg/labels"
 
 	"github.com/k8ssandra/k8ssandra-operator/test/kustomize"
 	"github.com/rs/zerolog"
@@ -323,7 +324,7 @@ func beforeTest(t *testing.T, f *framework.E2eFramework, fixtureDir string, opts
 		return err
 	}
 
-	if err := f.WaitForCassOperatorToBeReady(opts.operatorNamespace, polling.operatorDeploymentReady.timeout, polling.operatorDeploymentReady.interval); err != nil {
+	if err := f.WaitForCassOperatorToBeReady("cass-operator", polling.operatorDeploymentReady.timeout, polling.operatorDeploymentReady.interval); err != nil {
 		t.Log("failed waiting for cass-operator to be ready")
 		return err
 	}
